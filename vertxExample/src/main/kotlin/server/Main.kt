20de46c869d063a302a2eb8c3106c9c93fd3eb05
zhengye.zhang <zhengye.zhang@owitho.com>
2018-05-05 14:22:35 +0800

    start vertx with kotlin

---
+++ vertxExample/src/main/kotlin/server/Main.kt
@@ -0,0 +1,14 @@
+package server
+
+import io.vertx.core.Vertx
+import verticle.MyVerticle
+
+/**
+ * @Author: zhengye.zhang
+ * @Description:
+ * @Date: 2018/5/5 下午2:21
+ */
+fun main(args: Array<String>) {
+    val vertx = Vertx.vertx()
+    vertx.deployVerticle(MyVerticle().javaClass.name)
+}
---
+++ vertxExample/src/main/kotlin/verticle/MyVerticle.kt
@@ -0,0 +1,29 @@
+package verticle
+
+import io.vertx.core.AbstractVerticle
+
+/**
+ * @Author: zhengye.zhang
+ * @Description:
+ * @Date: 2018/5/5 下午2:21
+ */
+class MyVerticle : AbstractVerticle() {
+
+    var i = 0
+
+    override fun start() {
+        vertx.createHttpServer()
+                .requestHandler { req ->
+                    req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x, and val: " + i)
+                }
+                .listen(8080)
+
+        vertx.createHttpServer()
+                .requestHandler { req ->
+                    i++
+                    req.response().end()
+                }
+                .listen(8081)
+    }
+
+}
