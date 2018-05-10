cd0e05358f35d394c4cfa100e3fe814b13d70cd4
zhengye.zhang <zhengye.zhang@owitho.com>
2018-05-10 15:47:09 +0800

    handler

--- vertxExample/src/main/kotlin/web/BodyHandlerVerticle.kt
+++ vertxExample/src/main/kotlin/web/BodyHandlerVerticle.kt
@@ -1,8 +1,12 @@
 package web
 
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.http.HttpMethod
 import io.vertx.ext.web.Router
-import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.ext.web.handler.*
+import io.vertx.ext.web.sstore.LocalSessionStore
+import io.vertx.ext.web.sstore.SessionStore
+import io.vertx.ext.web.templ.TemplateEngine
 
 /**
  * @Author: zhengye.zhang
@@ -18,13 +22,21 @@ class BodyHandlerVerticle : AbstractVerticle() {
 
         router.route().handler(BodyHandler.create())
 
+        //router.route().handler(CookieHandler.create())
+
+        //router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx)))
+
+        //router.route().handler(StaticHandler.create())
+
+        //router.route().handler(CorsHandler.create("vertx\\.io").allowedMethod(HttpMethod.GET))
+
+        router.route().handler(TimeoutHandler.create(5000))
 
         router.post("body").handler({ context ->
             val body = context.bodyAsJson.map
             body.forEach({ (key, value) ->
                 println("key:$key, value:$value")
             })
-
         })
 
 
---
+++ vertxExample/src/main/kotlin/web/client/Webclient.kt
@@ -0,0 +1,9 @@
+package web.client
+
+/**
+ * @Author: zhengye.zhang
+ * @Description:
+ * @Date: 2018/5/10 下午3:46
+ */
+class Webclient{
+}
