7326d94a5e0ca04c6dd60b87bcd2fec2401ef16f
zhengye.zhang <zhengye.zhang@owitho.com>
2018-05-12 22:08:32 +0800

    web client

--- vertxExample/src/main/kotlin/web/client/Webclient.kt
+++ vertxExample/src/main/kotlin/web/client/Webclient.kt
@@ -1,28 +1,62 @@
 package web.client
 
+import io.vertx.core.AbstractVerticle
+import io.vertx.ext.web.client.WebClient
+import io.vertx.ext.web.client.WebClientOptions
+import io.vertx.core.json.JsonObject
+
+
+
+
 /**
  * @Author: zhengye.zhang
  * @Description:
  * @Date: 2018/5/10 下午3:46
  */
-class Webclient(val age:Int, val name: String){
+
+class Webclient : AbstractVerticle() {
+
+    override fun start() {
+        val options = WebClientOptions()
+                .setUserAgent("My-App/1.2.3")
+        options.isKeepAlive = false
+        val webClient = WebClient.create(vertx, options)
+
+        webClient//.head()
+                .get(8080, "myserver.mycompany.com", "/some-uri")
+                .send({ ar ->
+                    if (ar.succeeded()) {
+                        // 获取响应
+                        val response = ar.result()
+                        println(response.bodyAsJsonObject())
+                    } else {
+                        println("err")
+                    }
+                })
+
+        webClient.head(8080, "myserver.mycompany.com", "/some-uri")
+                .putHeader("content-type", "application/json")
+                .putHeader("other-header", "foo")
+                .addQueryParam("param1", "param_value1")
+                .addQueryParam("param2", "param_value2")
+                /*.putHeader("content-length", fileLen)
+                   .sendStream(fileStream, ar -> {
+                   if (ar.succeeded()) {
+                       // Ok
+                   }
+                });*/
+                //.sendBuffer()
+                .sendJson(hashMapOf("one" to 1), {})
+                /*.sendJsonObject(JsonObject()
+                        .put("firstName", "Dale")
+                        .put("lastName", "Cooper"), {})*/
+               //.send({})
+
+    }
+
 
 }
 
 fun main(args: Array<String>) {
-    var map: HashMap<Int, Int> = hashMapOf(1 to 11, 2 to 12, 3 to 13)
 
-    var aa = hashMapOf(1 to 11, 2 to 12, 3 to 13)
-
-    map = aa
-
-
-    listOf<Int>().last { true }
-
-
-
-}
-
-fun getInt():Int?{
-    return null
 }
