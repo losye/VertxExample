ec29854add240239115e8b0f3c6ecb4690a9f79c
zhengye.zhang <zhengye.zhang@owitho.com>
2018-06-30 17:25:44 +0800

    add base verticle

---
+++ vertxExample/src/main/java/micro/BaseVerticle.java
@@ -0,0 +1,116 @@
+package micro;
+
+import io.vertx.circuitbreaker.CircuitBreaker;
+import io.vertx.circuitbreaker.CircuitBreakerOptions;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.impl.ConcurrentHashSet;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.servicediscovery.Record;
+import io.vertx.servicediscovery.ServiceDiscovery;
+import io.vertx.servicediscovery.ServiceDiscoveryOptions;
+import io.vertx.servicediscovery.types.EventBusService;
+import io.vertx.servicediscovery.types.HttpEndpoint;
+import io.vertx.servicediscovery.types.JDBCDataSource;
+import io.vertx.servicediscovery.types.MessageSource;
+
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * @Author: zhengye.zhang
+ * @Description:
+ * @Date: 2018/6/30 下午3:54
+ */
+public class BaseVerticle extends AbstractVerticle {
+
+    private static final String LOG_EVENT_ADDRESS = "events.log";
+
+    private static final Logger logger = LoggerFactory.getLogger(BaseVerticle.class);
+
+    protected ServiceDiscovery discovery;
+    protected CircuitBreaker circuitBreaker;
+    protected Set<Record> registeredRecords = new ConcurrentHashSet<>();
+
+    @Override
+    public void start() throws Exception {
+        discovery = ServiceDiscovery.create(vertx, new ServiceDiscoveryOptions().setBackendConfiguration(config()));
+
+        JsonObject cbOptions = Optional.ofNullable(config().getJsonObject("circuit-breaker")).orElse(new JsonObject());
+        circuitBreaker = CircuitBreaker.create(cbOptions.getString("name", "circuit-breaker"),
+                vertx,
+                new CircuitBreakerOptions()
+                        .setMaxFailures(cbOptions.getInteger("max-failures", 5))
+                        .setTimeout(cbOptions.getLong("timeout", 30000L))
+                        .setFallbackOnFailure(true)
+                        .setResetTimeout(cbOptions.getLong("reset-timeout", 300000L))
+        );
+
+
+
+    }
+
+    protected Future<Void> publishHttpEndpoint(String name, String host, int port) {
+        Record record = HttpEndpoint.createRecord(name, host, port, "/",
+                new JsonObject().put("api.name", config().getString("api.name", "")));
+        return publish(record);
+    }
+
+    protected Future<Void> publishApiGateway(String host, int port) {
+        Record record = HttpEndpoint.createRecord("api-gateway", true, host, port, "/", null).setType("api-gateway");
+        return publish(record);
+    }
+
+    protected Future<Void> publishMessageSource(String name, String address) {
+        Record record = MessageSource.createRecord(name, address);
+        return publish(record);
+    }
+
+    protected Future<Void> publishJDBCDataSource(String name, JsonObject location) {
+        Record record = JDBCDataSource.createRecord(name, location, new JsonObject());
+        return publish(record);
+    }
+
+    protected Future<Void> publishEventBusService(String name, String address, Class serviceClass) {
+        Record record = EventBusService.createRecord(name, address, serviceClass);
+        return publish(record);
+    }
+
+    protected void publishLogEvent(String type, JsonObject data) {
+        JsonObject msg = new JsonObject().put("type", type)
+                .put("message", data);
+        vertx.eventBus().publish(LOG_EVENT_ADDRESS, msg);
+    }
+
+    protected void publishLogEvent(String type, JsonObject data, boolean succeeded) {
+        JsonObject msg = new JsonObject().put("type", type)
+                .put("status", succeeded)
+                .put("message", data);
+        vertx.eventBus().publish(LOG_EVENT_ADDRESS, msg);
+    }
+
+    private Future<Void> publish(Record record) {
+        if (discovery == null){
+            try {
+                start();
+            } catch (Exception e) {
+                throw new IllegalStateException("Cannot create discovery service");
+            }
+        }
+
+        Future<Void> future = Future.future();
+        discovery.publish(record, ar -> {
+            if (ar.succeeded()) {
+                registeredRecords.add(record);
+                logger.info(String.format("service: %s is published", ar.result().getName()));
+                future.complete();
+            } else {
+                future.fail(ar.cause());
+            }
+        });
+        return future;
+    }
+
+}
